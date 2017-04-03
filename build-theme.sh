98b8c2a151021b6cfbe0345df51943123edf90ff
Jason Corbett <jasoncorbett@gmail.com>
2017-04-03 08:50:21 -0600

    change to use downloaded node-sass

--- build-theme.sh
+++ build-theme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node-sass --include-path node_modules/bulma --include-path node_modules theme.sass >theme.css
+./node_modules/.bin/node-sass --include-path node_modules/bulma --include-path node_modules theme.sass >theme.css
