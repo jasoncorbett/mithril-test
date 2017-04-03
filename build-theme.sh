1ba6d72bb1a0c89b600d8c1a62c403fb35c08cd8
Jason Corbett <jasoncorbett@gmail.com>
2017-04-03 11:38:32 -0600

    Theme changing integrated

--- build-theme.sh
+++ build-theme.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./node_modules/.bin/node-sass --include-path node_modules/bulma --include-path node_modules theme.sass >theme.css
+./node_modules/.bin/node-sass --include-path node_modules/bulma --include-path node_modules light.sass >light.css
+./node_modules/.bin/node-sass --include-path node_modules/bulma --include-path node_modules dark.sass >dark.css
